32dd3f2f2d77d963cc7f24876fc19abb1102dbf4
Glenn-Accomplice <glenn@willingaccomplice.com.au>
2018-03-05 16:45:46 +1000

    making the home transition something a little less obnoxious

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="container">
     <h1 class="mb-4">The below is from a Firebase Database</h1>
-    <div class="sk-folding-cube" v-if="headings.length == 0">
-      <div class="sk-cube1 sk-cube"></div>
-      <div class="sk-cube2 sk-cube"></div>
-      <div class="sk-cube4 sk-cube"></div>
-      <div class="sk-cube3 sk-cube"></div>
-    </div>
 
     <transition-group appear
       name="custom-classes-transition"
-      enter-active-class="animated bounce"
-      v-else>
+      enter-active-class="animated fadeInUpBig">
       <div v-for="(item, key) in headings" :key="key">
-        <h2>{{ item.heading }}</h2>
-        <h3>{{ item.subheading }}</h3>
+        <h2 v-text="item.heading"></h2>
+        <h3 v-text="item.subheading"></h3>
         <div class="row" v-html="item.content"></div>
       </div>
     </transition-group>
@@ -23,7 +16,6 @@
 
 <script>
 import {db} from './firebase/get-content'
-import '../assets/spinners.css'
 import '../assets/animate.css'
 
 export default {
